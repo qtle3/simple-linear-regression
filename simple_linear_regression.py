55cd4241b4eb0bbb59f8c2e62fc95b31e0f8d44b
Quocviet Le <quocvietlet@gmail.com>
2024-08-10 19:55:04 -0500

    plot the training and testing sets

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -9,6 +9,9 @@ dataset = pd.read_csv("Salary_Data.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+print(dataset)
+print(X)
+print(y)
 # split the dataset into training and test set
 from sklearn.model_selection import train_test_split
 
@@ -24,3 +27,17 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # visualize the training set results
+plt.scatter(X_train, y_train, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="green")
+plt.title("Salary vs Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+# visualize the test set results
+plt.scatter(X_test, y_test, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="green")
+plt.title("Salary vs Experience (Test Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
