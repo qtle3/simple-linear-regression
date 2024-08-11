933281546eca809763efb048560b25887e9c7aad
Quocviet Le <quocvietlet@gmail.com>
2024-08-10 19:11:27 -0500

    train the simple linear regression model

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -13,3 +13,14 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# train the simple linear regression model on the training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# predict the test set results
+y_pred = regressor.predict(X_test)
+
+# visualize the training set results
